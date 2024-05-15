510c34c2af6a00f68700a1a4d2423dcbaa6e7461
Jasvinder Singh <jasvinder4251@gmail.com>
2024-05-15 19:44:12 +0530

    add login and register

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const port = 3000;
 
 app.use(express.static('public'))
 
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+})
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+})
+
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
