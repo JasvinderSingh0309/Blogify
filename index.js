3f92fc2909f239bb548c3925bb5c75eb2951be7b
Jasvinder Singh <jasvinder4251@gmail.com>
2024-05-16 18:09:35 +0530

    set routes and display users blogs

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'))
+env.config();
+
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+});
+db.connect();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/login", (req, res) => {
   res.render("login.ejs");
@@ -14,8 +29,53 @@ app.get("/register", (req, res) => {
 })
 
 app.get("/", (req, res) => {
+  // get all data from the user-Blog db and send to index.ejs.
   res.render("index.ejs");
 });
+
+app.post("/login",async (req, res) => {
+  console.log(req.body);
+  let email = req.body.email;
+  let password = req.body.password;
+
+  let hasEmail = await db.query("SELECT * FROM users WHERE email = $1",[email]);
+
+  if(hasEmail.rows.length) {
+    let passwordFromDB = hasEmail.rows[0].password;
+
+    if(passwordFromDB === password) {
+      //get all thi users blog from user-Blog db and sent it while rendering blog.ejs
+      res.render("blog.ejs"
+      // , {blogs:"",}
+      )
+    }else{
+      res.render("login.ejs", {
+        message: "Please enter correct password.",
+      });
+    }
+  }else{
+    res.render("register.ejs", {
+      message: "Please register to continue.",
+    });
+  }
+})
+
+app.post("/register",async (req, res) => {
+  console.log(req.body);
+  let email = req.body.email;
+  let password = req.body.password;
+
+  let hasEmail = await db.query("SELECT * FROM users WHERE email = $1",[email]);
+
+  if(!hasEmail.rows.length) {
+    await db.query("INSERT INTO users (email, password) VALUES ($1, $2)",[email, password]);
+    res.redirect("/login");
+  }else{
+    res.render("register.ejs", {
+      message: "Email already exists. Please try again!!",
+    });
+  }
+})
 
 app.listen(port, () => {
   console.log(`Server is listening at port ${port}`);
